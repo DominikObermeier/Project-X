8c789fa64126cddcececa9d02b96ad7197b66fd4
KyroxCG <dominikobermeier@gmx.de>
2021-09-12 14:19:10 +0200

    Added Design Settings in SettingsWindow and implemented design to MainMenuWindow. Implemented methods for changing colorResources. Added CheckBoxes for DarkMode and Default which checks automatically the current design mode.

--- Project-X/Settings.xaml.cs
+++ Project-X/Settings.xaml.cs
@@ -20,6 +20,12 @@ namespace Project_X
     public partial class Settings : Window
     {
         bool saved = false;
+        bool designDefault = false;
+        bool designDarkMode = false;
+
+
+        SolidColorBrush colorBackground;
+        SolidColorBrush colorText;
         public Settings()
         {
             InitializeComponent();
@@ -41,6 +47,68 @@ namespace Project_X
                     // If user doesn't want to close, cancel closure
                     e.Cancel = true;
                 }
+            }
+        }
+
+        private void Design_Default_RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            designDefault = true;
+        }
+
+        private void Design_Default_RadioButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            designDefault = false;
+        }
+        private void Design_DarkMode_RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            designDarkMode = true;
+        }
+        private void Design_DarkMode_RadioButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            designDarkMode = false;
+        }
+
+        private void Settings_SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Save Design Settings
+            if (designDefault == true)
+            {
+                colorBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#E5E5E5"));
+
+                Application.Current.Resources["BackgroundColor"] = colorBackground;
+
+                colorText = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#000000"));
+
+                Application.Current.Resources["ForegroundColor"] = colorText;
+            } else if (designDarkMode == true)
+            {
+                colorBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF212121"));
+
+                Application.Current.Resources["BackgroundColor"] = colorBackground;
+
+                colorText = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFFFF"));
+
+                Application.Current.Resources["ForegroundColor"] = colorText;
+            }
+
+            saved = true;
+        }
+
+        private void SettingsWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.Resources["BackgroundColor"].ToString() == "#E5E5E5")
+            {
+                designDefault = true;
+            }else if (Application.Current.Resources["BackgroundColor"].ToString() == "#FF212121")
+            {
+                designDarkMode = true;
+            }
+            if (designDarkMode == true)
+            {
+                Design_DarkMode_RadioButton.IsChecked = true;
+            }else if (designDefault == true)
+            {
+                Design_Default_RadioButton.IsChecked = true;
             }
         }
     }
